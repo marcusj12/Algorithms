8f1eef9d02146c95eb7a368a5c60b1b3b0a47c74
marcusj12 <marcus.a.jennings12@gmail.com>
2022-04-07 20:11:03 -0400

    Day 1: Data Types & scanner class usage

---
+++ src/com/MarcusJennings/Edabit/ArrayOfMultiples.java
@@ -0,0 +1,18 @@
+package com.MarcusJennings.Edabit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayOfMultiples {
+    public static void main(String[] args) {
+            List<Integer> arrayList = new ArrayList<>();
+            int num1 = 2;
+            int num2 = 4;
+            arrayList.add(num1,num2);{
+                for(int i = 0; i >= arrayList.size();i++){
+                    System.out.println(arrayList.get(i));
+                }
+                }
+
+        }
+    }
---
+++ src/com/MarcusJennings/Hackerrank/Day05Loops.java
@@ -0,0 +1,7 @@
+package com.MarcusJennings.Hackerrank;
+
+public class Day05Loops {
+    public static void main(String[] args) {
+
+    }
+}
