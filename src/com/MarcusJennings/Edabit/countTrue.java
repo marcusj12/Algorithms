ffb3e0b01fd9ede19fb0cab5c23f6f7a9b50cb48
marcusj12 <marcus.a.jennings12@gmail.com>
2022-04-11 21:28:37 -0400

    Count True Algorithm

--- src/com/MarcusJennings/Edabit/countTrue.java
+++ src/com/MarcusJennings/Edabit/countTrue.java
@@ -1,5 +1,17 @@
 package com.MarcusJennings.Edabit;
 
 public class countTrue {
+    public static void main(String[] args) {
+        System.out.println(countTrue(new boolean[]{true, false, true, true})) ;
+    }
+    public static int countTrue(boolean[] arr) {
+        int counter = 0;
+        for (int i =0; i < arr.length; i++){
+            if (arr[i] == true) counter++;
+        }
+        return counter;
+
+    }
+
 
 }
